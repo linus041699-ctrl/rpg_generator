be93a3c73b7e6012183019c47eecb187bc8aeed6
Jan Linus <linus041699@gmail.com>
2026-06-03 10:42:52 +0000

    Initial commit: Add generator.py skeleton

---
+++ generator.py
@@ -0,0 +1,8 @@
+def main():
+print("---Welcome to the Welcome to the Character Generator ---")
+    # Components will be combined here
+    name = "Hero"
+    print(f"\nCharacter {name} has been successfully created!")
+
+if __name__ == "__main__":
+    main()
