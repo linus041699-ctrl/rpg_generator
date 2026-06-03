597593f6a10c7b1a978a80c1d4ee762caf984ecc
Hammy <mohammad.mahmoud@benilde.edu.ph>
2026-06-03 19:22:10 +0800

    Add choose_class function for class selection

--- generator.py
+++ generator.py
@@ -1,3 +1,8 @@
+def choose_class():
+    classes = ["Warrior", "Mage", "Rogue"]
+    print("Available Classes:", ", ".join(classes))
+    choice = input("Choose your class: ").capitalize()
+    return choice if choice in classes else "Adventurer"
 def main():
 print("---Welcome to the Welcome to the Character Generator ---")
     # Components will be combined here
